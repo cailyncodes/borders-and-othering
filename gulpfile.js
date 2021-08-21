6bf2b24608638e738818581b7ad729453fcaedb0
Cailyn Hansen <cailyn.e.hansen@gmail.com>
2021-08-21 18:34:29 -0400

    Upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,162 @@
-'use strict';
+"use strict";
 // Generated on 2019-05-04 using generator-leaflet 0.0.17
 
-var gulp = require('gulp');
-var open = require('open');
-var wiredep = require('wiredep').stream;
+var gulp = require("gulp");
+var open = require("open");
 
 // Load plugins
-var $ = require('gulp-load-plugins')();
+var $ = require("gulp-load-plugins")();
 
 // Styles
-gulp.task('styles', function () {
-    return gulp.src(['app/resources/css/*.css'])
-        // .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('app/styles'))
-        .pipe($.size());
-});
+const styles = function () {
+  return (
+    gulp
+      .src(["app/resources/css/*.css"])
+      // .pipe($.autoprefixer('last 1 version'))
+      .pipe(gulp.dest("app/styles"))
+      .pipe($.size())
+  );
+};
 
 // Scripts
-gulp.task('scripts', function () {
-    return gulp.src(['app/resources/js/*.js'])
-        // .pipe($.jshint('.jshintrc'))
-        // .pipe($.jshint.reporter('default'))
-        .pipe(gulp.dest('app/scripts'))
-        .pipe($.size());
-});
+const scripts = function () {
+  return (
+    gulp
+      .src(["app/resources/js/*.js"])
+      // .pipe($.jshint('.jshintrc'))
+      // .pipe($.jshint.reporter('default'))
+      .pipe(gulp.dest("app/scripts"))
+      .pipe($.size())
+  );
+};
 
 // GeoStyles
-gulp.task('geostyles', function () {
-    return gulp.src(['app/resources/geostyle/*.json'])
-        // .pipe($.jshint('.jshintrc'))
-        // .pipe($.jshint.reporter('default'))
-        .pipe(gulp.dest('app/geostyles'))
-        .pipe(gulp.dest('dist/geostyle'))
-        .pipe($.size());
-});
+const geostyles = function () {
+  return (
+    gulp
+      .src(["app/resources/geostyle/*.json"])
+      // .pipe($.jshint('.jshintrc'))
+      // .pipe($.jshint.reporter('default'))
+      .pipe(gulp.dest("app/geostyles"))
+      .pipe(gulp.dest("dist/geostyle"))
+      .pipe($.size())
+  );
+};
 
-// HTML
-gulp.task('html', ['styles', 'scripts', 'geostyles'], function () {
-    var jsFilter = $.filter('app/scripts/*.js');
-    var cssFilter = $.filter('app/styles/*.css');
-    // var geoStyleFilter = $.filter('app/geostyle/*.json');
+// HTML=
+const html = function () {
+  var jsFilter = $.filter("app/scripts/*.js", { restore: true });
+  var cssFilter = $.filter("app/styles/*.css", { restore: true });
+  // var geoStyleFilter = $.filter('app/geostyle/*.json');
 
-    return gulp.src('app/*.html')
-        .pipe($.useref.assets())
-        .pipe(jsFilter)
-        .pipe($.uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore())
-        // .pipe(geoStyleFilter)
-        // .pipe(geoStyleFilter.restore())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
-});
+  const assets = $.useref.assets();
+
+  return (
+    gulp
+      .src("app/*.html")
+      .pipe(assets)
+      .pipe(jsFilter)
+      .pipe($.uglify())
+      .pipe(jsFilter.restore)
+      .pipe(cssFilter)
+      .pipe($.csso())
+      .pipe(cssFilter.restore)
+      // .pipe(geoStyleFilter)
+      // .pipe(geoStyleFilter.restore())
+      .pipe(assets.restore())
+      .pipe($.useref())
+      .pipe(gulp.dest("dist"))
+      .pipe($.size())
+  );
+};
 
 // Images
-gulp.task('images', function () {
-    return gulp.src([
-    		'app/resources/images/*',
-            'app/lib/images/*',
-            'app/images/*'])
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
-});
+const images = function () {
+  return gulp
+    .src(["app/resources/images/*", "app/lib/images/*", "app/images/*"])
+    .pipe(gulp.dest("dist/images"))
+    .pipe($.size());
+};
 
 // Clean
-gulp.task('clean', function () {
-    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images', 'app/styles', 'app/scripts', 'app/geostyles'], { read: false }).pipe($.clean());
-});
-
-// Build
-gulp.task('build', ['html', 'images']);
-
-// Default task
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
+const clean = function () {
+  return gulp
+    .src(
+      [
+        "dist/styles",
+        "dist/scripts",
+        "dist/images",
+        "app/styles",
+        "app/scripts",
+        "app/geostyles",
+      ],
+      { read: false, allowEmpty: true }
+    )
+    .pipe($.clean());
+};
 
 // Connect
-gulp.task('connect', function(){
-    $.connect.server({
-        root: 'app',
-        port: 9000,
-        livereload: true
-    });
-});
+const connect = function () {
+  $.connect.server({
+    root: "app",
+    port: 9000,
+    livereload: true,
+  });
+};
+
+const reload = function () {
+  return gulp
+    .src([
+      "app/*.html",
+      "app/resources/css/*.css",
+      "app/resources/js/*.js",
+      "app/resources/images/*",
+      "app/resources/geostyle/*",
+    ])
+    .pipe($.connect.reload());
+};
 
 // Open
-gulp.task('serve', ['connect'], function() {
+const serve = function () {
   open("http://localhost:9000");
-});
-
-// Inject Bower components
-gulp.task('wiredep', function () {
-    gulp.src('app/styles/*.css')
-        .pipe(wiredep({
-            directory: 'app/bower_components',
-            ignorePath: 'app/bower_components/'
-        }))
-        .pipe(gulp.dest('app/styles'));
-
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components',
-            ignorePath: 'app/'
-        }))
-        .pipe(gulp.dest('app'));
-});
+};
 
 // Watch
-gulp.task('watch', ['connect', 'serve'], function () {
-    // Watch for changes in `app` folder
-    gulp.watch([
-        'app/*.html',
-        'app/resources/css/*.css',
-        'app/resources/js/*.js',
-        'app/resources/images/*',
-        'app/resources/geostyle/*'
-    ], function (event) {
-        return gulp.src(event.path)
-            .pipe($.connect.reload());
-    });
+const registerWatch = function () {
+  // Watch for changes in `app` folder
+  gulp.watch(
+    [
+      "app/*.html",
+      "app/resources/css/*.css",
+      "app/resources/js/*.js",
+      "app/resources/images/*",
+      "app/resources/geostyle/*",
+    ],
+    {
+        ignoreInitial: true
+    },
+    reload
+  );
 
-    // Watch .css files
-    gulp.watch('app/resources/css/*.css', ['styles']);
+  // Watch .css files
+  gulp.watch("app/resources/css/*.css", styles);
 
-    // Watch .js files
-    gulp.watch('app/resources/js/*.js', ['scripts']);
+  // Watch .js files
+  gulp.watch("app/resources/js/*.js", scripts);
 
-    // Watch .json files
-    gulp.watch('app/resources/geostyle/*.json', ['geostyles']);
+  // Watch .json files
+  gulp.watch("app/resources/geostyle/*.json", geostyles);
 
-    // Watch image files
-    gulp.watch('app/resources/images/*', ['images']);
+  // Watch image files
+  gulp.watch("app/resources/images/*", images);
 
-    // Watch bower files
-    gulp.watch('bower.json', ['wiredep']);
-});
+  // Watch bower files
+  gulp.watch("bower.json", wiredep);
+};
+
+const build = gulp.series(styles, scripts, geostyles, html, images);
+
+exports.build = build;
+exports.clean = clean;
+exports.watch = gulp.parallel(connect, serve, registerWatch);
+exports.default = gulp.series(clean, build);
